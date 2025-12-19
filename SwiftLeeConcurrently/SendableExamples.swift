794dc4103bc54ebe0b17fc199b9a7c1737d78277
lfcj <fernanda16400@gmail.com>
2025-12-19 13:16:54 +0100

    New code that should produce warnings in Xcode

--- SwiftLeeConcurrently/SendableExamples.swift
+++ SwiftLeeConcurrently/SendableExamples.swift
@@ -1,3 +1,14 @@
-class PersonViewModel {
+public class PersonViewModel {
     var personName: String! = "Default"
+}
+
+public class BankAccount {
+    var saldo: Int = 0
+}
+
+public struct Person: Sendable {
+    let name: String
+    let account: BankAccount
+
+    var saldo: Int { account.saldo }
 }
